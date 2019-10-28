076bb55758a9d6e2337744c471668e4c1521f9ce
Paulose <pauloseraj@hotmail.com>
2019-10-28 18:32:57 +0530

    Initial Commit

---
+++ src/com/javatestdata/structure/TestClassDs.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.javatestdata.structure;
+
+/**
+ * @author paulose
+ *
+ */
+public class TestClassDs {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
